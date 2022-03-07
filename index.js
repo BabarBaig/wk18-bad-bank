5bf42dd5d3bcbae1642caf332b2f37edafec9cf3
Babar Baig <BobBaig@outlook.com>
2022-03-06 20:05:44 -0500

    add user context to files

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function Spa() {
   return (
     <HashRouter>
-      <NavBar/>
-      <Route path="/"         exact component={Home} />
-      <Route path="/alldata/"       component={AllData} />
-      <Route path="/balance/"       component={Balance} />
-      <Route path="/createaccount/" component={CreateAccount} />
-      <Route path="/deposit/"       component={Deposit} />
-      <Route path="/login/"         component={Login} />
-      <Route path="/withdraw/"      component={Withdraw} />
+      <NavBar />
+      <UserContext.Provider value={{users:[{name:'abel',
+            email:'abel@mit.edu', password:'secret', balance:100}]}}>
+        <Route path="/" exact component={Home} />
+        <Route path="/alldata/" component={AllData} />
+        <Route path="/balance/" component={Balance} />
+        <Route path="/createaccount/" component={CreateAccount} />
+        <Route path="/deposit/" component={Deposit} />
+        <Route path="/login/" component={Login} />
+        <Route path="/withdraw/" component={Withdraw} />
+      </UserContext.Provider>
     </HashRouter>
   )
 }
 
-ReactDOM.render(<Spa/>, document.getElementById('root'))
+ReactDOM.render(<Spa />, document.getElementById('root'))
