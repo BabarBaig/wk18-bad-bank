1a30162f4f9465454be2efa579683da38b43bda5
Babar Baig <BobBaig@outlook.com>
2022-03-10 15:15:31 -0500

    add .gitignore

--- alldata.js
+++ alldata.js
@@ -1,15 +1,15 @@
 function AllData() {
-  // const ctx = React.useContext(UserContext)
-  // return (<h1>All Data<br/>{JSON.stringify(ctx)}</h1>)
+  const ctx = React.useContext(UserContext)
+  return (<h1>All Data<br/>{JSON.stringify(ctx)}</h1>)
   // {"users":[{"name":"abel","email":"abel@mit.edu","password":"secret","All Data":100}]}
-  return (
-    <Card
-      bgcolor ="primary"
-      txtcolor="white"
-      header  ="All Data"
-      title   ="All Data in Account"
-      text    ="All Data in your account with Bad Bank"
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />
-  )
+  // return (
+  //   <Card
+  //     bgcolor ="primary"
+  //     txtcolor="white"
+  //     header  ="All Data"
+  //     title   ="All Data in Account"
+  //     text    ="All Data in your account with Bad Bank"
+  //     body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+  //   />
+  // )
 }
